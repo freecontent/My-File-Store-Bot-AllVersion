0a46eefbeb01bf60625f606f56b76230eb13b7ed
freecontent <84922944+freecontent@users.noreply.github.com>
2021-07-20 15:22:32 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -41,7 +41,7 @@ async def storefile(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/@temp_demo?start={base64_string}"
+    url = f"https://t.me/{bot.username}?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
@@ -96,7 +96,7 @@ async def storefile_channel(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/@temp_demo?start={base64_string}"
+    url = f"https://t.me/{bot.username}?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
